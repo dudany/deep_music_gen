00ab5bb1a4a23b955843d91d1e03aaa4457eeafe
Dani Dubinsky <dani.dubinsky@placer.ai>
2020-10-02 20:22:05 +0300

    get mapping method added

--- data_extraction.py
+++ data_extraction.py
@@ -1,11 +1,16 @@
 import os
-from typing import List
+from typing import List, Dict
 
 import music21
 from music21 import note, chord
 
 
 def run_data_extraction(path: str) -> List:
+    """
+    This function returns list of unvectorized prepared data
+    :param path:
+    :return:
+    """
     notes_data = []
     for i, f in enumerate(os.listdir(path)):
         file_p = os.path.join(path, f)
@@ -13,6 +18,17 @@ def run_data_extraction(path: str) -> List:
         midi_notes = extract_notes_from_midi(midi_file)
         notes_data.append(midi_notes)
     return notes_data
+
+
+def get_notes_mapping_dict(notes_list: List[List]) -> Dict:
+    assert len(notes_list) > 0, 'Empty notes list !!'
+    full_list = []
+    for songs_notes in notes_list:
+        for note_e in songs_notes:
+            full_list.append(note_e)
+    full_list = set(full_list)
+    mapping = {note_e: i for i, note_e in enumerate(full_list)}
+    return mapping
 
 
 def extract_notes_from_midi(midi_file: music21.stream.Stream) -> List:
--- local_exec.py
+++ local_exec.py
@@ -1,10 +1,10 @@
-from data_extraction import run_data_extraction
+from data_extraction import run_data_extraction, get_notes_mapping_dict
 from config import data_path
 
 
 def test_data_extraction():
     list_test = run_data_extraction(data_path)
-
+    mapping = get_notes_mapping_dict(list_test)
 
 if __name__ == '__main__':
     test_data_extraction()
